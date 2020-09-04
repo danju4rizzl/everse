735d879299c81716dbde8d7ad5584187f5f87b5d
danju4rizzl <danjumashiwaju@gmail.com>
2020-09-04 22:31:03 +0200

    added grid layout

--- main.js
+++ main.js
@@ -21,13 +21,31 @@ function print_verse_book(book) {
   return (DomStrings.book.textContent = book);
 }
 
+function saveContents(itemObject) {
+  localStorage.setItem('verseOfTheDay', JSON.stringify(itemObject));
+  const localVerse = localStorage.getItem('verseOfTheDay');
+  const storedVerseOfTheDay = JSON.parse(localVerse);
+
+  print_verse_content(storedVerseOfTheDay.verse);
+  print_verse_book(storedVerseOfTheDay.book);
+}
+
 (() => {
   axios
     .get(proxyUrl + url)
     .then(function (response) {
       let { text, reference, version } = response.data.verse.details;
-      print_verse_content(text);
-      print_verse_book(reference);
+
+      // Remember to replace this object with real api calls
+      const verseInfo = {
+        verse:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, quo!',
+        book: 'Lorem ipsum',
+      };
+
+      // print_verse_content(text);
+      // print_verse_book(reference);
+      saveContents(verseInfo);
     })
     .catch(function (error) {
       console.log(error);

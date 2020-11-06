a957d649935722438ba34bdf847dba8433f5c91e
danju4rizzl <danjumashiwaju@gmail.com>
2020-11-06 10:55:46 +0200

    dateTimeWidget- house keeping

--- src/widgets/dateTimeWidget.js
+++ src/widgets/dateTimeWidget.js
@@ -1,7 +1,7 @@
 import { domStrings } from '../appSettings';
 import { formattedMonth, storeContents } from '../utils';
 /*
-  To handle the current date
+  To handle the dateTimeWidget
  */
 
 export function dateTimeWidget() {
@@ -11,24 +11,8 @@ export function dateTimeWidget() {
   let h = date.getHours(); // 0 - 23
   let m = date.getMinutes(); // 0 - 59
   let s = date.getSeconds(); // 0 - 59
-  // let month = new Array();
-  // month[0] = 'January';
-  // month[1] = 'February';
-  // month[2] = 'March';
-  // month[3] = 'April';
-  // month[4] = 'May';
-  // month[5] = 'June';
-  // month[6] = 'July';
-  // month[7] = 'August';
-  // month[8] = 'September';
-  // month[9] = 'October';
-  // month[10] = 'November';
-  // month[11] = 'December';
-
-  // month[date.getMonth()]
 
   const months = formattedMonth(date.getMonth());
-  console.log(typeof months);
 
   const { currentTime, currentDate, greetings } = domStrings.dateTimeBox;
 
